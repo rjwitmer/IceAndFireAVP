a3f03c6b73f25bb7d39b1f9ed12c019c8106d5b3
Bob Witmer <bwitmer@golden.net>
2025-11-05 09:11:02 -0500

    Initial Commit

---
+++ IceAndFireAVP/IceAndFireAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceAndFireAVPApp.swift
+//  IceAndFireAVP
+//
+//  Created by Bob Witmer on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct IceAndFireAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
